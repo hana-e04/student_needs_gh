2a270486407f7ea18e8b885d91d03ca44b03d204
Hana Lee <hanaeubinlee@gmail.com>
2025-04-03 16:51:54 -0400

    clean interim 2024 market date

--- get_sn/dataset.py
+++ get_sn/dataset.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import csv
+
 from pathlib import Path
 
 from loguru import logger
@@ -12,7 +15,7 @@ app = typer.Typer()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
     input_path: Path = "/Users/hanalee/student_needs_repo/data/external/2024marketdata_ext.csv",
-    output_path: Path = "/Users/hanalee/student_needs_repo/data/interim/student_spending_int.csv",
+    output_path: Path = "/Users/hanalee/student_needs_repo/data/interim/2024marketdata_int.csv",
     # ----------------------------------------------
 ):
     # ---- REPLACE THIS WITH YOUR OWN CODE ----
@@ -23,9 +26,30 @@ def main(
     logger.success("Processing dataset complete.")
     # -----------------------------------------
 
-main()
 #checks if the current script is being run directly as the main program, 
     # or if it's being imported as a module into another program
 if __name__ == "__main__":
     # show the run time of command in terminal
     app()
+
+market_2024 = pd.read_csv("../data/external/2024marketdata_ext.csv")
+
+# desired column indices
+df_index = [0, 1, 16, 18, 19, 24, 25, 26, 
+            28, 31, 40, 50, 56, 57, 58]
+
+# renaming selected columns
+new_col = ['month', 'retail_total', 'furniture', 'appliances',
+            'electronics', 'food_stores', 'grocery', 'supermarket',
+            'hp_care', 'clothes', 'books', 'stationary',
+            'food_service', 'restaurants', 'ls_eating']
+
+# create dataframe with desired columns
+market_df_uncleaned = pd.DataFrame()
+for i in range(len(new_col)):
+    market_df_uncleaned[new_col[i]] = market_2024.iloc[:, df_index[i]]
+
+# export new, cleaned dataset to interim data folder
+with open("../data/interim/2024marketdata_int.csv", 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(market_df_uncleaned)
